054781fb459349bf9da21d09b87a9befddb6853d
Muscardina <vergonzalezsainz@gmail.com>
2024-06-17 13:41:56 +0200

    a comer

--- Public/24-express-parametros/main.js
+++ Public/24-express-parametros/main.js
@@ -15,26 +15,22 @@ function generarArrayBidimensional() {
 }
 
 function mostrarArrayBidimensional(arrayBidimensional) {
-
-
   function mostrarArray(array) {
     var temporal = "";
     array.forEach(function (elemento) {
       var formatedElemento = (" " + elemento).slice(-2);
-      temporal += ("<td>" + elemento + "</td>")
+      temporal += "<td>" + elemento + "</td>";
     });
     console.log("|");
 
-    return temporal
+    return temporal;
   }
-
 
   document.getElementById("table").innerHTML += "<tr>";
   arrayBidimensional.forEach(function (array) {
     document.getElementById("table").innerHTML += "<tr>";
-    document.getElementById("table").innerHTML += mostrarArray(array); 
+    document.getElementById("table").innerHTML += mostrarArray(array);
     document.getElementById("table").innerHTML += "</tr>";
-
   });
   document.getElementById("table").innerHTML += "</tr>";
 }
